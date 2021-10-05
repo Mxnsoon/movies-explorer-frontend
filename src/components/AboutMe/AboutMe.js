817f8e7e459253ff393939ddbef9b7780962884d
Mxnsoon <nigga.guy@yandex.ru>
2021-10-05 06:19:28 +0300

    fixes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,5 @@
 import './AboutMe.css';
-import photo from '../../images/student-photo.png';
+import photo from '../../images/student-photo.jpg';
 
 function AboutMe() {
     return (
@@ -7,10 +7,10 @@ function AboutMe() {
             <h2 className="about-me__title">Студент</h2>
             <div className="about-me__container">
                 <div className="about-me__info">
-                    <h3 className="about-me__name">Виталий</h3>
-                    <p className="about-me__description">Фронтенд-разработчик, 30 лет</p>
-                    <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
-и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+                    <h3 className="about-me__name">Сергей</h3>
+                    <p className="about-me__description">Фронтенд-разработчик, 26 лет</p>
+                    <p className="about-me__text">Я родился и живу в Воронеже. Мне нравится фронтенд, и все, что с ним связано.
+                        Каждый день я обучаюсь чему то новому, и интересному. В свободное время люблю активные виды спорта, а так же компьютерные игры.
                     </p>
                     <div className="about-me__links">
                         <a href="https://facebook.com/" className="about-me__link">Facebook</a>
